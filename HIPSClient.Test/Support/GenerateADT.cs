854f758ef8ddd9229b00cceaf4591eb1de79cb08
angusmillar <angusbmillar@gmail.com>
2019-03-28 13:22:48 +1000

    Added Sall Biscuit ADT A01 test case

--- HIPSClient.Test/Support/GenerateADT.cs
+++ HIPSClient.Test/Support/GenerateADT.cs
@@ -142,5 +142,72 @@ namespace HIPSClient.Test.Support
 
       return ADT;
     }
+
+    public ADT A01_SallyBiscuit()
+    {
+      var ADT = new ADT();
+      ADT.Patient = new Patient();
+      ADT.Patient.Family = "Biscuit";
+      ADT.Patient.Given = "Sally";
+      ADT.Patient.Title = "Ms";
+      ADT.Patient.DateOfBirth = new DateTime(1984, 04, 04);
+      ADT.Patient.Gender = Gender.Female;
+
+      ADT.Patient.IndigenousStatus = new IndigenousStatus()
+      {
+        IndigenousStatusType = IndigenousStatusType.NotStatedInadequatelyDescribed
+      };
+
+      ADT.Patient.Address = new Address()
+      {
+        AddressLineOne = "5 Cook Street",
+        AddressLineTwo = "",
+        Suburb = "Red Hill",
+        PostCode = "4059",
+        State = "QLD",
+        Country = "AUS"
+      };
+
+      //ADT.Patient.HomeContact = new Contact()
+      //{
+      //  Value = "93235615"
+      //};
+
+      //ADT.Patient.WorkContact = new Contact()
+      //{
+      //  Value = "0414778341"
+      //};
+
+      ADT.Patient.IdentifierList = new List<PatientIdentifier>();
+
+      //MRN
+      ADT.Patient.IdentifierList.Add(new PatientIdentifier()
+      {
+        Value = "000000102",
+        Type = PatientIdentifierType.MedicalRecordNumber,
+        AssigningAuthority = Common.HIPS.HipsConfig.HospitalCode
+      });
+
+      //Medicare Number
+      ADT.Patient.IdentifierList.Add(new PatientIdentifier()
+      {
+        Value = "49504816511",
+        Type = PatientIdentifierType.MedicareNumber
+      });
+
+
+      ADT.HospitalEncounter = new HospitalEncounter()
+      {
+        PatientClass = PatientClassType.InPatient,
+        VisitNumber = "000005",
+        Bed = "Bed",
+        Room = "Room",
+        Ward = "Ward",
+        AdmissionDate = new DateTime(2019, 03, 18),
+        DischargeDate = null
+      };
+
+      return ADT;
+    }
   }
 }
--- HIPSClient.Test/Test_ADT.cs
+++ HIPSClient.Test/Test_ADT.cs
@@ -44,5 +44,23 @@ namespace HIPSClient.Test
       //Assert
       Assert.IsTrue(Response.IsSuccess);
     }
+
+    [TestMethod]
+    public void TestADT_A01_SallyBiscuit()
+    {
+      //Prepare
+      var GenerateADT = new Support.GenerateADT();
+      var DatabaseLoader = new DatabaseLoaderClient();
+
+      //Act
+      var Response = DatabaseLoader.ADT(new DatabaseLoaderRequest()
+      {
+        EventType = HL7EventType.A01,
+        ADT_A01 = GenerateADT.A01_SallyBiscuit()
+      });
+
+      //Assert
+      Assert.IsTrue(Response.IsSuccess);
+    }
   }
 }
